e0dcff100a7c3d5d4b3f5a66569d65f68b26d13d
Juan Pablo Caballero <juan.ccastellanos@mail.escuelaing.edu.co>
2025-09-08 11:40:58 -0500

    Taller : Juan Caballero - Estructura de carpetas y pom.xml

---
+++ src/main/java/edu/dosw/lab/Taller_Evaluativo/TallerEvaluativoApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Taller_Evaluativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerEvaluativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerEvaluativoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/lab/Taller_Evaluativo/TallerEvaluativoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.lab.Taller_Evaluativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerEvaluativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
